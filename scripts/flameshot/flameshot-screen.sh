93e99f7147ae8bae8c34bf19fa7f6b107e3077af
Nathan Guerrero <naguerr2@uci.edu>
2025-08-11 03:20:38 +0000

    Fix how directory is inferred for Flameshot common script

--- scripts/flameshot/flameshot-screen.sh
+++ scripts/flameshot/flameshot-screen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./flameshot-common.sh
+source $(dirname "$0")/flameshot-common.sh
 
 # NOTE: In COSMIC, screen number is currently bugged! Only does monitor 0 instead of where cursor is
 flameshot screen $FLAMESHOT_ARGS --path "$SCREENSHOT_FULL_DIR/$SCREENSHOT_FILE_NAME"
